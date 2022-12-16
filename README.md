19674328b759a949ee1489f8d5156ad35f57e59f
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2022-12-16 17:33:33 -0300

    feat: domain modeling

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# Python Project Template
+# FastAPI Project Template
 
-A Project Template based on Cosmic Python
+A Project Template based on [Cosmic Python](https://www.cosmicpython.com/) `Allocation` service example.
+
+## Context
+
+![Context](https://www.cosmicpython.com/book/images/apwp_0102.png)
+
+> At a high level, we have separate systems that are responsible for buying stock, selling stock to customers, and
+> shipping goods to customers. A system in the middle needs to coordinate the process by allocating stock to a
+> customer’s
+> orders.
+>
+> <cite>[Cosmic Python - Chapter 1](https://www.cosmicpython.com/book/chapter_01_domain_model.html#allocation_context_diagram)</cite>
+
---
+++ src/allocation/domain/models.py
@@ -0,0 +1,96 @@
+"""Models.
+ This module contains the models used by the allocation service.
+"""
+
+from dataclasses import dataclass
+from datetime import date
+
+
+@dataclass(frozen=True)
+class OrderLine:
+    """
+    Represents an item in an order.
+    """
+    order_id: str
+    sku: str
+    qty: int
+
+
+class Batch:
+    """
+    Represents Batch of a Stock.
+    """
+
+    def __init__(self, ref: str, sku: str, qty: int, eta: date | None = None):
+        self.reference: str = ref
+        self.sku: str = sku
+        self.eta: date | None = eta
+        self._purchased_quantity = qty
+        self._allocations: set[OrderLine] = set()
+
+    def allocate(self, line: OrderLine):
+        """
+        Reduces the available quantity by the quantity of the line.
+
+        Args:
+            line (OrderLine): The line to allocate.
+        """
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        """
+        Increases the available quantity by the quantity of the line.
+
+        Args:
+            line (OrderLine): The line to deallocate.
+        """
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        """
+        Checks if the batch can allocate the line.
+
+        Args:
+            line (OrderLine): The line to allocate.
+
+        Returns:
+            bool: True if the batch can allocate the line.
+        """
+        return self.sku == line.sku and self.available_quantity >= line.qty
+
+    @property
+    def allocated_quantity(self) -> int:
+        """
+        Sums the quantities of the allocated lines.
+
+        Returns:
+            int: the quantity allocated.
+        """
+        return sum(line.qty for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        """
+        Calculates the difference between purchased and allocated.
+
+        Returns:
+            int: the available quantity.
+        """
+        return self._purchased_quantity - self.allocated_quantity
+
+    def __eq__(self, other):
+        if not isinstance(other, Batch):
+            return False
+        return other.reference == self.reference
+
+    def __hash__(self):
+        return hash(self.reference)
+
+    def __gt__(self, other):
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        return self.eta > other.eta
