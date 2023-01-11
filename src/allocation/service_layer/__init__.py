73f4d351990c34337e52e75b610fe6f96e40f34f
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2023-01-11 19:35:35 -0300

    feat(service): add allocation service

---
+++ src/allocation/service_layer/__init__.py
@@ -0,0 +1,2 @@
+"""Business case layer. This layer is the heart of the application. It contains the business logic, implemented by
+functions and objects. Also, it can include service-layer exceptions """
