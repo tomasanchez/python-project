48d2a6f562e4037aef45f6717a50e91dcaf2f1ea
Tomas Sanchez <tosanchez@frba.utn.edu.ar>
2023-01-11 19:31:31 -0300

    refactor: update using service layer

--- src/allocation/routers.py
+++ src/allocation/routers.py
@@ -6,9 +6,10 @@ from logging.config import dictConfig
 
 from fastapi import APIRouter, Depends, HTTPException
 
+from allocation.adapters import repository
 from allocation.dependencies import get_batch_repository
 from allocation.domain import models, schemas
-from allocation.repository import repository
+from allocation.service_layer.allocation_service import AllocationService, InvalidSku, NoBatchesAvailable, OutOfStock
 from allocation.settings.config import LogConfig
 
 router = APIRouter(prefix="/v1", tags=["allocation"])
@@ -18,20 +19,24 @@ logger = logging.getLogger("allocation_service")
 
 
 @router.post("/allocate", status_code=201)
-def allocate(order: schemas.OrderLine, batch_repository: repository.AbstractRepository = Depends(get_batch_repository)):
+def allocate(
+    order: schemas.OrderLine,
+    batch_repository: repository.AbstractRepository = Depends(get_batch_repository),
+):
     """
     Allocates an order line.
     """
     logger.info("Allocating order (%s)", order)
     line = models.OrderLine(order.order_id, order.sku, order.qty)
-    batches: list[models.Batch] = batch_repository.find_all()
 
-    for batch in batches:
-        if batch.can_allocate(line):
-            batch.allocate(line)
-            batch_repository.save(batch)
-            logger.info("Allocated batch (%s)", batch)
-            return {"reference": batch.reference}
+    try:
+        service = AllocationService(batch_repository)
+        batch_ref = service.allocate(line)
+    except (InvalidSku, OutOfStock) as e:
+        logger.error("Could not allocate for order (%s)", order)
+        raise HTTPException(status_code=400, detail=str(e)) from e
+    except NoBatchesAvailable as e:
+        logger.warning("No batches available")
+        raise HTTPException(status_code=404, detail=str(e)) from e
 
-    logger.error("Could not allocate for order (%s)", order)
-    raise HTTPException(status_code=404, detail="Could not allocate. No batches found")
+    return {"reference": batch_ref}
